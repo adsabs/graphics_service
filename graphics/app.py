f51e598f7faefa2b193118afd84024428c373b12
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-11-14 16:04:55 -0500

    removed example routes

--- graphics/app.py
+++ graphics/app.py
@@ -1,15 +1,12 @@
 import os
 from flask import Flask, g
-from views import blueprint, Resources, UnixTime, PrintArg, ExampleApiUsage, Graphics, DisplayGraphics
+from views import blueprint, Resources, Graphics, DisplayGraphics
 from flask.ext.restful import Api
 from client import Client
 
 def create_app():
   api = Api(blueprint)
   api.add_resource(Resources, '/resources')
-  api.add_resource(UnixTime, '/time')
-  api.add_resource(PrintArg,'/print/<string:arg>')
-  api.add_resource(ExampleApiUsage,'/search')
   api.add_resource(Graphics, '/<string:bibcode>')
   api.add_resource(DisplayGraphics,'/<string:bibcode>/<string:figure_id>/<string:image_format>')
 
--- graphics/views.py
+++ graphics/views.py
@@ -13,7 +13,7 @@ blueprint = Blueprint(
 
 class Graphics(Resource):
     """"Return graphics information for a given bibcode"""
-    scopes = 'oauth:graphics:read'
+    scopes = []
     def get(self, bibcode):
        try:
            results = get_graphics(bibcode)
@@ -23,13 +23,12 @@ class Graphics(Resource):
 
 class DisplayGraphics(Resource):
     """Return image data for a given figure"""
-    scopes = 'oauth:displaygraphics:read'
+    scopes = []
     def get(self,bibcode,figure_id,image_format):
         format2ext = {'tb':'gif','lr':'jpg','hr':'png'}
         image_ext = format2ext.get(image_format,'png')
         image_dir = current_app.config['IMAGE_PATH'] + ptree.id2ptree(bibcode)
         image = "%s%s_%s_%s.%s" % (image_dir,bibcode,figure_id,image_format,image_ext)
-        print image
         try:
             image_data = open(image, "rb").read()
         except Exception, e:
@@ -40,7 +39,7 @@ class DisplayGraphics(Resource):
 
 class Resources(Resource):
   '''Overview of available resources'''
-  scopes = ['oauth:sample_application:read','oauth_sample_application:logged_in']
+  scopes = []
   def get(self):
     func_list = {}
     for rule in current_app.url_map.iter_rules():
@@ -49,27 +48,3 @@ class Resources(Resource):
                               'description': current_app.view_functions[rule.endpoint].view_class.__doc__,
                               }
     return func_list, 200
-
-class UnixTime(Resource):
-  '''Returns the unix timestamp of the server'''
-  scopes = ['oauth:sample_application:read','oauth_sample_application:logged_in']
-  def get(self):
-    return {'now': time.time()}, 200
-
-class PrintArg(Resource):
-  '''Returns the :arg in the route'''
-  scopes = ['oauth:sample_application:read','oauth:sample_application:logged_in'] 
-  def get(self,arg):
-    return {'arg':arg}, 200
-
-class ExampleApiUsage(Resource):
-  '''This resource uses the app.client.session.get() method to access an api that requires an oauth2 token, such as our own adsws'''
-  scopes = ['oauth:sample_application:read','oauth:sample_application:logged_in','oauth:api:search'] 
-  def get(self):
-    r = current_app.client.session.get('http://api.adslabs.org/v1/search')
-    try:
-      r = r.json()
-      return {'response':r, 'api-token-which-should-be-kept-secret':current_app.client.token}, 200
-    except: #For the moment, 401s are not JSON encoded; this will be changed in the future
-      r = r.text
-      return {'raw_response':r, 'api-token-which-should-be-kept-secret':current_app.client.token}, 501
