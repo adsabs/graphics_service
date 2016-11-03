20e317a77520f62db0b08c2cd12fec9b4cde1584
Edwin Henneken <ehenneken@cfa.harvard.edu>
2016-11-03 06:34:02 -0400

    Added A&A thumbnails

--- service/config.py
+++ service/config.py
@@ -8,6 +8,7 @@ GRAPHICS_ENABLE_UPDATES = False
 # The key defines the set and the values are journals (or categories, in the
 # case of arXiv)
 GRAPHICS_PUBSETS = {
+                   'EDP':['A&A'],
                    'IOP':['ApJ','ApJL','ApJS','AJ'],
                    'Elsevier':['NewA'],
                    'arXiv': ['arXiv', 'acc-phys', 'adap-org', 'alg-geom',
@@ -23,19 +24,24 @@ GRAPHICS_PUBSETS = {
 GRAPHICS_EXTSOURCES = ['IOP', 'Elsevier']
 # Some info for the external site
 GRAPHICS_HEADER = {
+                  'EDP':'Every image links to the article on <a href="http://www.aanda.org/" target="_new">Astronomy &amp; Astrophysics</a>',
                   'IOP':'Every image links to the <a href="http://www.astroexplorer.org/" target="_new">AAS "Astronomy Image Explorer"</a> for more detail.',
                   'Elsevier':'Every image links to the article on <a href="http://www.sciencedirect.com" target="_new">ScienceDirect</a>'
                   }
 # Define the mapping to help retrieve full text files for a given identifier
 GRAPHICS_FULLTEXT_MAPS = {
+    'EDP':'/path/to/EDP.map',
     'IOP':'/path/to/IOP.map',
     'arXiv':'/path/to/arXiv.map'
 }
+# Location of local graphics files
+GRAPHICS_GRAPHICS_LOCATION = {}
 # Define a file with backdata, if available
 GRAPHICS_BACK_DATA_FILE = {
 }
 # These are the values to be stored as "source" in the graphics database
 GRAPHICS_SOURCE_NAMES = {
+    'EDP': 'EDP',
     'IOP': 'IOP',
     'Elsevier':'Elsevier',
     'arXiv': 'arXiv',
@@ -53,6 +59,12 @@ GRAPHICS_SOLR_PATH = 'https://api.adsabs.harvard.edu/v1/search/query'
 # This section configures this application to act as a client, for example
 # to query solr via adsws
 GRAPHICS_API_TOKEN = 'we will provide an api key token for this application'
+# To communicate with AWS
+GRAPHICS_AWS_ACCESS_KEY = 'this will be provided through local_config.py'
+GRAPHICS_AWS_SECRET_KEY = 'this will be provided through local_config.py'
+GRAPHICS_AWS_REGION = 'this will be provided through local_config.py'
+GRAPHICS_AWS_S3_URL = 'https://s3.amazonaws.com'
+GRAPHICS_AWS_S3_BUCKET = ''
 # Config for logging
 GRAPHICS_LOGGING = {
     'version': 1,
